7a08e2549147631078dca4bc1deaa5dad9fa8c58
Dipesh Patel <mr.dvpatel@gmail.com>
2019-12-07 11:51:55 -0500

    Fix PID logic for rotation.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorControllerEx.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorControllerEx.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
 public class MotorControllerEx {
 
     private PIDController pidDrive;
 
     private PIDController pidRotate;
+
+    private boolean enableRotatePidCalled;
 
     //  private double power = .30;
     //  private double correction ;
@@ -23,7 +22,6 @@ public class MotorControllerEx {
         // P by itself may stall before turn completed so we add a bit of I (integral) which
         // causes the PID controller to gently increase power if the turn is not completed.
         this.pidRotate = new PIDController(Constants.PID_ROTATE_KP, Constants.PID_ROTATE_KI, Constants.PID_ROTATE_KD);
-
     }
 
     public void enableDrivePID(double power) {
@@ -36,13 +34,39 @@ public class MotorControllerEx {
 
 
     public void enableRotatePID(double degrees, double power) {
+
+        // if degrees > 359 we cap at 359 with same sign as original degrees.
+        if (Math.abs(degrees) > 359) degrees = (int) Math.copySign(359, degrees);
+
+        // start pid controller. PID controller will monitor the turn angle with respect to the
+        // target angle and reduce power as we approach the target angle. This is to prevent the
+        // robots momentum from overshooting the turn after we turn off the power. The PID controller
+        // reports onTarget() = true when the difference between turn angle and target angle is within
+        // 1% of target (tolerance) which is about 1 degree. This helps prevent overshoot. Overshoot is
+        // dependant on the motor and gearing configuration, starting power, weight of the robot and the
+        // on target tolerance. If the controller overshoots, it will reverse the sign of the output
+        // turning the robot back toward the setpoint value.
+
         this.pidRotate.reset();
         this.pidRotate.setSetpoint(degrees);
         this.pidRotate.setInputRange(0, degrees);
         this.pidRotate.setOutputRange(0, power);
-        this.pidRotate.setTolerance(1);
+        this.pidRotate.setTolerance(0.5);
         this.pidRotate.enable();
+
+        this.enableRotatePidCalled = true;
     }
+
+    //  Customize pid for drive ;
+    public void setDrivePID(double rkp, double rki, double rkd) {
+        this.pidDrive = new PIDController(rkp, rki, rkd);
+    }
+
+    //  Customize pid for rotation ;
+    public void setRotatePID(double rkp, double rki, double rkd) {
+        this.pidRotate = new PIDController(rkp, rki, rkd);
+    }
+
 
     public void disablePID(){
         this.pidDrive.disable();
@@ -59,13 +83,15 @@ public class MotorControllerEx {
     }
 
 
-    public double[] calculateRotateCorrection(double degrees, double angle, double power) {
-        this.enableRotatePID(degrees, power) ;
-        double powerCorrection = this.pidRotate.performPID(angle);
+    public double calculateRotateCorrection(double degrees, double angle, double power) {
 
-        double[] corrections = {(power - powerCorrection), (power + powerCorrection), powerCorrection};
+        //  Must call this before opModeActive
+        if (!this.enableRotatePidCalled) {
+            this.enableRotatePID(degrees, power);
+            this.enableRotatePidCalled = true;
+        }
 
-        return corrections;
+        return this.pidRotate.performPID(angle);
     }
 
     public boolean angleOnTarget() {
