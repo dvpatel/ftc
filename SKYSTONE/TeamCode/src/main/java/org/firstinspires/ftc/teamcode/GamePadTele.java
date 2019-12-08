04e904af542ae80a505546ab9235ce9f2e774229
Dipesh Patel <mr.dvpatel@gmail.com>
2019-12-07 22:16:08 -0500

    Minor changes.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadTele.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadTele.java
@@ -5,7 +5,7 @@ package org.firstinspires.ftc.teamcode;
 //  https://drive.google.com/file/d/0B5ci5zMS_2kZUlRYaHZkMGNuZGc/view
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "GamePadTele", group = "Tele")
+@TeleOp(name = "GamePadDrive", group = "Tele")
 //  @Disabled
 public class GamePadTele extends AbstractLinearOpMode {
 
