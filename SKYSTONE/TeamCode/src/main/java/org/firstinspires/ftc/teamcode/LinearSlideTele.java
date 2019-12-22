453365d057de81fd0cca754de139f4f18ded030e
Dipesh Patel <mr.dvpatel@gmail.com>
2019-12-22 11:20:17 -0500

    LinearSlideTest update.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTele.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTele.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.blueprint.ftc.core.AbstractLinearOpMode;
 import org.blueprint.ftc.core.GamepadDriver;
@@ -8,27 +10,25 @@ import org.blueprint.ftc.core.ServoController;
 import org.blueprint.ftc.core.SimpleMotor;
 
 @TeleOp(name = "LinearSlideTest", group = "Linear Opmode")
-//@Disabled
+//  @Disabled
 public class LinearSlideTele extends AbstractLinearOpMode {
 
     private SimpleMotor linearSlideMotor;
     private ServoController linearSlideServo;
     private ServoController linearArmServo;
 
-    double power = this.normalizePower(0.50);
+    private static final int DISTANCE_IN_INCHES = 24;
+    private static final int SLEEP_TIME = 500;
+    private static final double POWER_LEVEL = 0.90;
 
     @Override
     public void initOpMode() throws InterruptedException {
-
-        telemetry.addData("Mode", "init Rosie");
-        telemetry.update();
-
         //  Make sure Rosie is initialized ;
         this.initRosie();
 
         this.linearSlideMotor = this.rosie.getLinearSlideMotor();
         this.linearSlideServo = this.rosie.getLinearSlideServo();
-        this.linearArmServo = this.rosie.getLinearArmServo();
+        //  this.linearArmServo = this.rosie.getLinearArmServo();
 
         telemetry.addData("Mode", "init complete;  Running");
         telemetry.update();
@@ -37,8 +37,8 @@ public class LinearSlideTele extends AbstractLinearOpMode {
     @Override
     public void stopOpMode() {
         this.linearSlideMotor.stop();
-        this.linearArmServo.setPositionByDegrees(180);
-        this.linearSlideServo.setPositionByDegrees(180);
+        this.linearSlideServo.setPositionByDegrees(0);
+        //  this.linearArmServo.setPositionByDegrees(180);
     }
 
     // called when init button is  pressed.
@@ -55,21 +55,34 @@ public class LinearSlideTele extends AbstractLinearOpMode {
         //  Tells motor to run to target using position and power ;  Make sure t reset encoder when done!
         //  this.simpleMotor.setTargetPosition(2500);
 
-        this.linearSlideMotor.setRunWithoutEncoderMode();
-        this.linearSlideMotor.setStopAndResetMode();
-        this.linearSlideMotor.setTicksToTargets(2);
-        this.linearSlideMotor.power(power);
-        this.linearSlideMotor.setRunToPositionMode();
-        while (this.linearSlideMotor.motorsBusy()) {
-            telemetry.addData("CurrentPosition", this.linearSlideMotor.getCurrentPosition());
-            telemetry.update();
-        }
-        this.linearSlideMotor.stop();
-        this.linearSlideMotor.setStopAndResetMode();
+        //  make sure power is between -1 and 1 ;
+        telemetry.addData("LinearSlide:  ", "forward");
+        telemetry.update();
+        this.linearSlideDriveForward(DISTANCE_IN_INCHES, POWER_LEVEL);
+        sleep(SLEEP_TIME);
+
+        this.linearSlideServo.setPositionByDegrees(0);
+        telemetry.addData("LinearSlideServoStart:  ", this.linearSlideServo.getPosition());
+        telemetry.update();
+        sleep(SLEEP_TIME);
+        this.linearSlideServo.setPositionByDegrees(180);
+        telemetry.addData("LinearSlideServoMid:  ", this.linearSlideServo.getPosition());
+        telemetry.update();
+        sleep(SLEEP_TIME);
+        this.linearSlideServo.setPositionByDegrees(0);
+        telemetry.addData("LinearSlideServoEnd:  ", this.linearSlideServo.getPosition());
+        telemetry.update();
+        sleep(SLEEP_TIME);
+
+        telemetry.addData("LinearSlide:  ", "reverse");
+        telemetry.update();
+        this.linearSlideDriveReverse(DISTANCE_IN_INCHES, POWER_LEVEL);
+        sleep(SLEEP_TIME);
+
 
         // motorsBusy / current position only works when using encoder wires;
         while (opModeIsActive()) {
-            this.linearSlideMotor.power(-gamepad1.left_stick_y);
+            this.linearSlideMotor.drive(-gamepad1.left_stick_y);
 
             telemetry.addData("CurrentPosition", this.linearSlideMotor.getCurrentPosition());
             telemetry.update();
