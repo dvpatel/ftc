07a4c52602e6c676480b4f38f9ef5aa716fc2431
Dipesh Patel <mr.dvpatel@gmail.com>
2019-12-29 00:04:20 -0500

    Intake tele sample.

---
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTele.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.blueprint.ftc.core.AbstractLinearOpMode;
+import org.blueprint.ftc.core.GamepadDriver;
+import org.blueprint.ftc.core.IntakeSystem;
+
+@TeleOp(name = "IntakeSystemTest", group = "Linear Opmode")
+//  @Disabled
+public class IntakeTele extends AbstractLinearOpMode {
+
+    private IntakeSystem intakeSystem;
+
+    @Override
+    public void initOpMode() throws InterruptedException {
+        //  Make sure Rosie is initialized ;
+        this.initRosie();
+
+        this.intakeSystem = rosie.getIntakeSystem();
+
+        telemetry.addData("Mode", "init complete;  Running");
+        telemetry.update();
+    }
+
+    @Override
+    public void stopOpMode() {
+        this.intakeSystem.stop();
+    }
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        this.initOpMode();
+
+        //  Wait for start button ;
+        this.waitToPressStart();
+
+        while (opModeIsActive()) {
+            this.intakeSystem.power(-gamepad1.left_stick_y);
+        }
+
+        this.stopOpMode();
+    }
+
+}
