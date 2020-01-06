c281aa1af1f7f9343d2c41aefe22739bcadc4b31
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-05 23:00:02 -0500

    Use Velocity for encoder driving.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TargetDriveAuto.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TargetDriveAuto.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.blueprint.ftc.core.AbstractLinearOpMode;
+import org.blueprint.ftc.core.Constants;
 import org.blueprint.ftc.core.Driver;
 import org.blueprint.ftc.core.IMUController;
 import org.blueprint.ftc.core.MotorControllerEx;
@@ -19,7 +20,7 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
     private static final int DISTANCE_IN_INCHES = 36;
     private static final int TURN_ANGLE = 90;
     private static final int SLEEP_TIME = 500;
-    private static final double POWER_LEVEL = 1;
+    private static final double VELOCITY = Constants.DEFAULT_VELOCITY;
 
     @Override
     public void initOpMode() throws InterruptedException {
@@ -54,43 +55,40 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
         //  Activate opmode
         this.waitToPressStart();
 
-        //  make sure power is between -1 and 1 ;
-        double power = this.normalizePower(POWER_LEVEL);
-
         telemetry.addData("Forward:  ", "forward");
         telemetry.update();
-        this.driveForward(DISTANCE_IN_INCHES, power);
+        this.driveForward(DISTANCE_IN_INCHES, VELOCITY);
         sleep(SLEEP_TIME);
 
         telemetry.addData("GyroStrafe:  ", "strafe left");
         telemetry.update();
-        this.strafeLeft(24, power);
+        this.strafeLeft(24, VELOCITY);
         sleep(SLEEP_TIME);
 
         //  drive backward 12 inches ;
         telemetry.addData("Reverse:  ", "back 6 inches");
         telemetry.update();
-        this.driveReverse(DISTANCE_IN_INCHES, power);
+        this.driveReverse(DISTANCE_IN_INCHES, VELOCITY);
         sleep(SLEEP_TIME);
 
         //  Strafe 12 inches right ; both values must be negative ;
         telemetry.addData("GyroStrafe:  ", "strafe 6 inches right");
         telemetry.update();
-        this.strafeRight(24, power);
+        this.strafeRight(24, VELOCITY);
         sleep(SLEEP_TIME);
 
         //  Turn 90 degrees to the right
         telemetry.addData("GyroTurn:  right ", "90 degrees");
         telemetry.update();
-        this.turnRight(TURN_ANGLE, power);
+        this.turnRight(TURN_ANGLE, VELOCITY);
         telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
         telemetry.update();
         sleep(SLEEP_TIME);
 
-        //  Turn 90 degrees left;  note degrees direction and power
+        //  Turn 90 degrees left;  note degrees direction and velocity
         telemetry.addData("GyroTurn: left  ", "90 degrees??");
         telemetry.update();
-        this.turnLeft(TURN_ANGLE, power);
+        this.turnLeft(TURN_ANGLE, VELOCITY);
         telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
         telemetry.update();
 
