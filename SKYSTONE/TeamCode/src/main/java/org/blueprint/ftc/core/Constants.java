20763d5493b3f108e58b6574717983baf5b4de87
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-05 22:36:01 -0500

    Update with PIDF, maxVelocity values.

--- SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/Constants.java
+++ SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/Constants.java
@@ -1,9 +1,12 @@
 package org.blueprint.ftc.core;
 
+import com.acmerobotics.dashboard.config.Config;
+
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
 
+@Config
 public final class Constants {
 
     //  Component names configured in RobotDriver?
@@ -25,17 +28,27 @@ public final class Constants {
     public static final String IMU_NAME = "imu" ;
     public static final String SHORT_ARM_SERVO = "short_arm_servo";
 
-    public static final double DEFAULT_MAX_POWER = 1.0;
-    public static final double DEFAULT_MIN_POWER = -1.0;
-    public static final double DEFAULT_POWER = 0.30;
-
     //  For AndyMark motor;  1 rev = 1120 ticks ;
-    public static final int MOTOR_TICK_COUNT = 1120;
     public static final int WHEEL_DIAMETER = 4; // inches ;
     public static final int WHEEL_WIDTH = 2; // inches ;
 
-    public static final double TICK_DIAMETER_RATIO = (Constants.MOTOR_TICK_COUNT / (Math.PI * Constants.WHEEL_DIAMETER));
+    public static final int MOTOR_TICK_COUNT = 1120;   //  ticks per revoluion;
+    public static final int MOTOR_GEAR_RATIO = 40;  //  NeverRest 40; 40:1 gearbox
+    public static final int MOTOR_PPR = 280;  //  NeverRest 40; 40:1 gearbox;  40*7
+    public static final int MOTOR_NO_LOAD_RPM = 160;
 
+    public static int MOTOR_MAX_VELOCITY = 2900;  //  ticks per second;
+    public static int DEFAULT_VELOCITY = (int) 0.90*MOTOR_MAX_VELOCITY;  //  ticks per second;
+
+    //  Cycles PER ROTATION; 28
+    public static final int MOTOR_CYCLE_PER_ROTATION = MOTOR_TICK_COUNT / MOTOR_GEAR_RATIO;
+
+    //  From this explanation;  Pulse generated per second;  747
+    //  https://www.robotshop.com/community/forum/t/deducing-the-encoder-ticks/46430
+    //  http://www.revrobotics.com/content/docs/Encoder-Guide.pdf
+    public static final double MOTOR_PPS = (MOTOR_NO_LOAD_RPM * MOTOR_PPR)/60;
+
+    public static final double TICK_DIAMETER_RATIO = (Constants.MOTOR_TICK_COUNT / (Math.PI * Constants.WHEEL_DIAMETER));
 
     //  For AndyMark motor connected to linear slide system
     public static final int SIMPLE_WHEEL_DIAMETER = 2; // inches ;
@@ -45,26 +58,26 @@ public final class Constants {
     public static final int COLOR_RED=1 ;
     public static final int COLOR_GREEN=2 ;
     public static final int COLOR_BLUE=3 ;
-    public static final double COLOR_SENSOR_DEFAULT_POWER = 0.50;
 
     //  Hue:  340 <  20; sat: 0.6 -->  RED
     //  Hue:  200 - 275; sat: 0.6 -->  Blue
-    public static final float TARGET_COLOR_BLUE_HUE_LOW = 165;
-    public static final float TARGET_COLOR_BLUE_HUE_HIGH = 265;
+    public static float TARGET_COLOR_BLUE_HUE_LOW = 165;
+    public static float TARGET_COLOR_BLUE_HUE_HIGH = 265;
 
-    public static final float TARGET_COLOR_RED_HUE_LOW = 320;
-    public static final float TARGET_COLOR_RED_HUE_HIGH = 15;
+    public static float TARGET_COLOR_RED_HUE_LOW = 320;
+    public static float TARGET_COLOR_RED_HUE_HIGH = 15;
 
-    public static final float TARGET_COLOR_SATURATION=0.6f ;
+    public static float TARGET_COLOR_SATURATION=0.6f ;
 
     // sometimes it helps to multiply the raw RGB values with a scale factor
     // to amplify/attentuate the measured values.
-    public static final double SCALE_FACTOR = 255;
+    public static double SCALE_FACTOR = 255;
 
-    //  Will need to tune these for driving straight
-    public static double PID_DRIVE_KP = 0.05 ;
-    public static double PID_DRIVE_KI = 0 ;
-    public static double PID_DRIVE_KD = 0.3;
+    //  Will need to tune these for driving straight;  See MaxVelocityTest.java
+    public static double PID_DRIVE_KP = 1.1299 ;
+    public static double PID_DRIVE_KI = 0.11299 ;
+    public static double PID_DRIVE_KD = 0.0;
+    public static double PID_DRIVE_KF = 11.2990;
 
     //  Will need to tune these for turning;
     // Speed of wheels while slowing down
@@ -78,7 +91,7 @@ public final class Constants {
 
     public static final String VUFORIA_KEY =
             "ARBNGpP/////AAABmbLvzx0Qekiui2o+DSSa3YJIIuD7Q0UL6sLKYRh6/OCm/uvQLlRLPNs/o72itb3SXgG71435htgeXTLgMciuPUca8vG5BbLoR5k9K5L6pbe8XLD9VFAG4Llh55ETmOQzz+S7yyjN69HtY34ahSjsi4bzZzwrfeTrTsCPfa1ZTAdf6MxWbZ5yn6LKmanzxLbnmBiftmRbgVVtxeMbOdxPv/f2uxXWqnKEHz5/LDvoacDFVQwu07AnvUXk0cDRSKEObQs5lE+IjdxSbYMOHPYbJy9jWf+2tZURyVZF1atz0nHaW1yra8YXg0HYQvWDzkt9+2S831dsB25sJElDK4xLGFFb/GVCSFGfnjvRHgbvD1AP";
-    public static final boolean PHONE_IS_PORTRAIT = false;
+    public static boolean PHONE_IS_PORTRAIT = false;
     public static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
     public static final float MM_PER_INCHES = 25.4f;
 
