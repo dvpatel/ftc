efc67dfc5fad49e777d8e2837b0fb12f4084d268
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-07 02:11:50 -0500

    Adjust to use velocity.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TargetDriveAuto.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TargetDriveAuto.java
@@ -19,8 +19,7 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
 
     private static final int DISTANCE_IN_INCHES = 36;
     private static final int TURN_ANGLE = 90;
-    private static final int SLEEP_TIME = 500;
-    private static final double VELOCITY = Constants.DEFAULT_VELOCITY;
+    private static final int SLEEP_TIME = 250;
 
     @Override
     public void initOpMode() throws InterruptedException {
@@ -30,10 +29,6 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
         this.imu = this.rosie.getIMUController();
         this.motor = this.rosie.getMotorPID();
         this.driver = this.rosie.getDriver();
-
-        //  Enable PID Controller to track state
-        //  this.motor.enablePID();
-        //this.motor.enableDrivePID(power);
 
         telemetry.addData("Mode", "init complete;  Running");
         telemetry.update();
@@ -57,30 +52,30 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
 
         telemetry.addData("Forward:  ", "forward");
         telemetry.update();
-        this.driveForward(DISTANCE_IN_INCHES, VELOCITY);
+        this.driveForward(DISTANCE_IN_INCHES, Constants.DEFAULT_VELOCITY);
         sleep(SLEEP_TIME);
 
-        telemetry.addData("GyroStrafe:  ", "strafe left");
+        telemetry.addData("Strafe:  ", "strafe left");
         telemetry.update();
-        this.strafeLeft(24, VELOCITY);
+        this.strafeLeft(24, 0.50*Constants.DEFAULT_VELOCITY);
         sleep(SLEEP_TIME);
 
         //  drive backward 12 inches ;
         telemetry.addData("Reverse:  ", "back 6 inches");
         telemetry.update();
-        this.driveReverse(DISTANCE_IN_INCHES, VELOCITY);
+        this.driveReverse(DISTANCE_IN_INCHES, Constants.DEFAULT_VELOCITY);
         sleep(SLEEP_TIME);
 
         //  Strafe 12 inches right ; both values must be negative ;
-        telemetry.addData("GyroStrafe:  ", "strafe 6 inches right");
+        telemetry.addData("Strafe:  ", "strafe 6 inches right");
         telemetry.update();
-        this.strafeRight(24, VELOCITY);
+        this.strafeRight(24, 0.50*Constants.DEFAULT_VELOCITY);
         sleep(SLEEP_TIME);
 
         //  Turn 90 degrees to the right
         telemetry.addData("GyroTurn:  right ", "90 degrees");
         telemetry.update();
-        this.turnRight(TURN_ANGLE, VELOCITY);
+        this.turnRight(TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
         telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
         telemetry.update();
         sleep(SLEEP_TIME);
@@ -88,7 +83,7 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
         //  Turn 90 degrees left;  note degrees direction and velocity
         telemetry.addData("GyroTurn: left  ", "90 degrees??");
         telemetry.update();
-        this.turnLeft(TURN_ANGLE, VELOCITY);
+        this.turnLeft(TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
         telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
         telemetry.update();
 
