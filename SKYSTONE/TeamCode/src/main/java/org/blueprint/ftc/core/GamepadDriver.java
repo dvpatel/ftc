2905f0c08b7c1a8b27619463908a07543407c3d3
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-10 22:27:37 -0500

    Add directional driving logic.

--- SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/GamepadDriver.java
+++ SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/GamepadDriver.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.blueprint.ftc.core.Driver;
 import org.blueprint.ftc.core.GameBot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import static org.blueprint.ftc.core.Constants.DEADZONE;
 
@@ -16,12 +20,16 @@ import static org.blueprint.ftc.core.Constants.DEADZONE;
 public class GamepadDriver {
 
     private Driver driver;
+    private IMUController imu;
+    double lastYawAngle;
+
 
     double leftFront, leftBack, rightFront, rightBack;
 
     GameBot game = new GameBot();
 
-    public GamepadDriver(Driver driver) {
+    public GamepadDriver(Driver driver, IMUController imu) {
+        this.imu = imu;
         this.driver = driver;
         this.driver.setRunWithEncoderOffMode();
     }
@@ -31,6 +39,67 @@ public class GamepadDriver {
     }
 
     public void drive(Gamepad gamepad) {
+        this.holonomicDriving(gamepad);
+        //  this.fieldOrientedDriving(gamepad);
+    }
+
+    //  Test;  Gears keep slipping!
+    public void fieldOrientedDriving(Gamepad gamepad) {
+
+        double forward = -gamepad.left_stick_y;
+        double strafe = gamepad.left_stick_x;
+        double turn = gamepad.right_stick_x;
+
+        //  theta is yaw
+        //  x cos(theta) -y sin(theta)  --->  strafe = strafe cos(theta) - forward sin(theta)
+        //  x sin(theta) + y cos(theta) --->  forward = strafe sin(theta) + forward cos(theta)
+
+        //  means turning;  only update when turning;
+        double yawAngle = lastYawAngle;
+        if (turn != 0) {
+            yawAngle = this.imu.getAngle();   //  Accumulating
+            //  double deltaAngle = yawAngle - lastYawAngle;
+            //  if (Math.abs(deltaAngle) < Constants.ANGLE_DEADZONE)
+            //    yawAngle = lastYawAngle;
+        }
+
+
+        strafe = strafe * Math.cos(yawAngle) - forward * Math.sin(yawAngle);
+        forward = strafe * Math.sin(yawAngle) + forward * Math.cos(yawAngle);
+
+        //  lastYawAngle = yawAngle;
+
+        /* At this point, Joystick X/Y (strafe/forwrd) vectors have been */
+        /* rotated by the gyro angle, and can be sent to drive system */
+
+        //  Buffer;
+        if (Math.abs(forward) < DEADZONE) forward = 0;
+        if (Math.abs(strafe) < DEADZONE) strafe = 0;
+        if (Math.abs(turn) < DEADZONE) turn = 0;
+
+        double leftFront = forward + strafe + turn;
+        double rightFront = forward - strafe - turn;
+        double leftBack = forward - strafe + turn;
+        double rightBack = forward + strafe - turn;
+
+        //  Scale;
+        double max = Math.max(Math.max(Math.abs(leftFront), Math.abs(rightFront)), Math.max(Math.abs(leftBack), Math.abs(rightBack)));
+        if (max > 1) {
+            leftFront = leftFront / max;
+            rightFront = rightFront / max;
+            leftBack = leftBack / max;
+            rightBack = rightBack / max;
+        }
+
+        this.driver.velocityDifferential(
+                toVelocity(leftFront),
+                toVelocity(rightFront),
+                toVelocity(leftBack),
+                toVelocity(rightBack));
+
+    }
+
+    public void holonomicDriving(Gamepad gamepad) {
 
         double forward = -gamepad.left_stick_y;
         double sideways = gamepad.left_stick_x;
