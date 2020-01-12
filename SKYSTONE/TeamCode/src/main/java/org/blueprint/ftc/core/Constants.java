53c65c50c7344968495bff0c488d266be2f4d49f
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-11 20:04:25 -0500

    Add CR Params.

--- SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/Constants.java
+++ SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/Constants.java
@@ -18,8 +18,13 @@ public final class Constants {
     // Left and right motors for intake system
     public static final String INTAKE_LEFT_MOTOR = "intake_left_motor";
     public static final String INTAKE_RIGHT_MOTOR = "intake_right_motor";
+
     public static final String INTAKE_LEFT_SERVO = "intake_left_servo";
     public static final String INTAKE_RIGHT_SERVO = "intake_right_servo";
+    public static final boolean INTAKE_LEFT_SERVO_REVERSE = false;
+    public static final boolean INTAKE_RIGHT_SERVO_REVERSE = true;
+    public static final double INTAKE_LEFT_SERVO_INIT_POWER = 0.22;
+    public static final double INTAKE_RIGHT_SERVO_INIT_POWER = 0.23;
 
     public static final String LINEAR_SLIDE_MOTOR_NAME = "linear_slide_motor";
     public static final String LINEAR_SLIDE_SERVO = "linear_slide_servo";
