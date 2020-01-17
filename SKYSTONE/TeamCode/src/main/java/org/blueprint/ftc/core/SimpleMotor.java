86668a568a094ac7f9946cd25a90e55fae71d202
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-17 12:02:03 -0500

    Bug fixes..

--- SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/SimpleMotor.java
+++ SKYSTONE/TeamCode/src/main/java/org/blueprint/ftc/core/SimpleMotor.java
@@ -1,5 +1,6 @@
 package org.blueprint.ftc.core;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -16,10 +17,8 @@ public class SimpleMotor {
 
         this.motor = hardwareMap.dcMotor.get(deviceName);
         this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        this.setStopAndResetMode();
+        this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         this.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        this.drive(0);
     }
 
     //  Calculate ticks for given inches ;
@@ -31,49 +30,46 @@ public class SimpleMotor {
         return (int) (ticks / Constants.SIMPLE_TICK_DIAMETER_RATIO);
     }
 
-
-    public void setTargetPosition(double distanceInInches) {
-
-        //  Always reset;  starts at zero;
-        this.setStopAndResetMode();
-
-        //  set target
-        this.setTicksToTargets(distanceInInches);
-
-        //  Run to target position;
-        this.setRunToPositionMode();
-
-        //  Apply power, somewhere ;  MAKE sure to turn off encoder when done.
-    }
-
     //  Revisit;  Are all encoders needed?  Also should only one motor be used for calc?
     public boolean motorsBusy() {
         return this.motor.isBusy();
-    }
-
-    public void setStopAndResetMode() {
-        this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    public void setRunToPositionMode() {
-        this.motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    }
-
-    public void setTicksToTargets(double distanceInInches) {
-        int ticks = this.calculateTicks(distanceInInches);
-        this.motor.setTargetPosition(ticks);
     }
 
     public void drive(double power) {
         this.motor.setPower(power);
     }
 
-    public int drive(double distanceInInches, double power) {
-        this.setTargetPosition(distanceInInches);
-        this.drive(power);
-        while (this.motor.isBusy()) {
+    public int drive(LinearOpMode opMode, double distanceInInches, double pow) {
+
+        //  power direction makes slide go up or down;
+        double deltaPos = distanceInInches-this.calculateInches(this.motor.getCurrentPosition());
+        double power = Math.signum(deltaPos) * pow;
+
+        // for reset, set deltaPos to 0;
+        if (distanceInInches == 0) {
+            deltaPos = 0;
+            power = -1 * pow;
         }
-        this.stop();
+
+        //  This is our target in ticks; increasing or decreasing based on power;
+        int newPosition = Math.abs(this.calculateTicks(deltaPos));
+
+        //  going to the right, current position increasing, positive power;
+        this.drive(power);
+        if (power > 0) {
+            while(opMode.opModeIsActive() && this.motor.getCurrentPosition() <= newPosition) {
+                opMode.telemetry.addData("Going Up:  ", this.motor.getCurrentPosition());
+                opMode.telemetry.addData("Target:  ", newPosition);
+                opMode.telemetry.update();
+            }
+        } else {
+            while(opMode.opModeIsActive() && this.motor.getCurrentPosition() >= newPosition) {
+                opMode.telemetry.addData("Going Down:  ", this.motor.getCurrentPosition());
+                opMode.telemetry.addData("Target:  ", newPosition);
+                opMode.telemetry.update();
+            }
+        }
+         this.stop();
         return this.getCurrentPosition();
     }
 
