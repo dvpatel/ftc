75bafc08dae6ee4b69b9568d80683a3ce1dc1f60
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-17 12:01:43 -0500

    Test for LiftSystem.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftSystemTele.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftSystemTele.java
@@ -6,10 +6,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.blueprint.ftc.core.AbstractLinearOpMode;
 import org.blueprint.ftc.core.CRServoController;
+import org.blueprint.ftc.core.Constants;
 import org.blueprint.ftc.core.GamepadDriver;
 import org.blueprint.ftc.core.LiftSystem;
 import org.blueprint.ftc.core.ServoController;
 import org.blueprint.ftc.core.SimpleMotor;
+
+import java.util.Random;
 
 @TeleOp(name = "LiftSystemTest")
 //  @Disabled
@@ -18,14 +21,17 @@ public class LiftSystemTele extends AbstractLinearOpMode {
     private LiftSystem liftSystem;
     private static final int SLEEP_TIME = 2000;
 
+    private double randomHeight;
+
     @Override
     public void initOpMode() throws InterruptedException {
         //  Make sure Rosie is initialized ;
         this.initRosie();
         this.liftSystem = this.rosie.getLiftSystem();
+        this.liftSystem.setLinearOpMode(this);
 
+        this.randomHeight = this.randomDouble(0.5, (Constants.SIMPLE_WHEEL_MAX_DISTANCE * 0.95));
         telemetry.addData("Mode", "init complete;  Running");
-        telemetry.update();
     }
 
     @Override
@@ -38,35 +44,23 @@ public class LiftSystemTele extends AbstractLinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         this.initOpMode();
+        telemetry.update();
 
         GamepadDriver gpd = this.rosie.getGamepadDriver();
 
         //  Wait for start button ;
         this.waitToPressStart();
 
-        //  Tells motor to run to target using position and power ;  Make sure t reset encoder when done!
-        //  this.simpleMotor.setTargetPosition(2500);
+        telemetry.addData("LiftSystem:  ", this.randomHeight + " inches.");
+        this.liftSystem.lift(this.randomHeight);
+        telemetry.update();
+        sleep(5000);
 
         //  make sure power is between -1 and 1 ;
         telemetry.addData("LiftSystem:  ", "Pickup");
         telemetry.update();
         this.liftSystem.pickup();
-        sleep(SLEEP_TIME);
-
-        telemetry.addData("LiftSystem:  ", "Move back");
-        this.liftSystem.lift(12);
-        telemetry.update();
-        sleep(SLEEP_TIME);
-
-        telemetry.addData("LiftSystem:  ", "Move down");
-        this.liftSystem.lift(-12);
-        telemetry.update();
-        sleep(SLEEP_TIME);
-
-        telemetry.addData("LiftSystem:  ", "Release");
-        this.liftSystem.releaseObject();
-        telemetry.update();
-        sleep(SLEEP_TIME);
+        sleep(5000);
 
         telemetry.addData("LifeSystem:  ", "Back to base");
         this.liftSystem.backToBase();
@@ -75,6 +69,14 @@ public class LiftSystemTele extends AbstractLinearOpMode {
 
         this.stopOpMode();
     }
+
+
+    double randomDouble(double min, double max)
+    {
+        Random rand = new Random();
+        return rand.nextDouble()*(max-min) + min;
+    }
+
 
 
 }
