6457dff1018ab7512d9c38251399de5d2ad3a7d2
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-17 12:07:47 -0500

    Disable.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNavigationWebcam.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNavigationWebcam.java
@@ -55,7 +55,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
  */
 
 @TeleOp()
-//  @Disabled
+@Disabled
 public class VuforiaNavigationWebcam extends LinearOpMode {
 
     // IMPORTANT: If you are using a USB WebCam, you must select CAMERA_CHOICE = BACK; and PHONE_IS_PORTRAIT = false;
