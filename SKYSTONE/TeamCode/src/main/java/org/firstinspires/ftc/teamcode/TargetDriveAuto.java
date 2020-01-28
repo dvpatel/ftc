f3e8c0f21ed678ff9264cc025738fdf37f1d02d1
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-27 19:11:59 -0500

    Add telemetry.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TargetDriveAuto.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TargetDriveAuto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
@@ -9,17 +10,19 @@ import org.blueprint.ftc.core.Driver;
 import org.blueprint.ftc.core.IMUController;
 import org.blueprint.ftc.core.MotorControllerEx;
 
-@Autonomous(name = "TargetDrive")
-@Disabled
+@Config
+@Autonomous()
+//  @Disabled
 public class TargetDriveAuto extends AbstractLinearOpMode {
 
     private MotorControllerEx motor;
     private Driver driver;
     private IMUController imu;
 
-    private static final int DISTANCE_IN_INCHES = 48;
-    private static final int TURN_ANGLE = 90;
-    private static final int SLEEP_TIME = 5000;
+    public static int DISTANCE_IN_INCHES = 36;
+    public static int TURN_ANGLE = 90;
+    public static int VELOCITY = Constants.DEFAULT_VELOCITY;
+    public static int SLEEP_TIME = 5000;
 
     @Override
     public void initOpMode() throws InterruptedException {
@@ -39,69 +42,92 @@ public class TargetDriveAuto extends AbstractLinearOpMode {
         driver.stop();
     }
 
+    private void addTelemetry() {
+
+        telemetry.addData("Targeted Ticks:", driver.calculateTicks(DISTANCE_IN_INCHES));
+        telemetry.addData("Targeted Inches:", DISTANCE_IN_INCHES);
+
+        int[] currentPosition = driver.getCurrentPosition();
+        telemetry.addData("CurrentPosition", "%d, %d, %d, %d",
+                currentPosition[0], currentPosition[1], currentPosition[2], currentPosition[3]);
+
+        telemetry.addData("CurrentPosition Inches", "%.04f, %.04f, %.0f, %.0f",
+                driver.toInches(currentPosition[0]),
+                driver.toInches(currentPosition[1]),
+                driver.toInches(currentPosition[2]),
+                driver.toInches(currentPosition[3]));
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         //  Put common init logic here
         this.initOpMode();
 
         telemetry.addData("Calibration Status:", this.imu.getCalibrationStatus());
+        this.addTelemetry();
+
         telemetry.update();
 
         //  Activate opmode
         this.waitToPressStart();
 
-        telemetry.addData("Forward:  ", "forward");
-        telemetry.update();
-        this.driveForward(DISTANCE_IN_INCHES, Constants.DEFAULT_VELOCITY);
-        sleep(SLEEP_TIME);
+        if(opModeIsActive()) {
 
-        telemetry.addData("Strafe:  ", "strafe left");
-        telemetry.update();
-        this.strafeLeft(DISTANCE_IN_INCHES, 0.50 * Constants.DEFAULT_VELOCITY);
-        sleep(SLEEP_TIME);
+            telemetry.addData("Forward:  ", "forward");
+            this.driveForward(DISTANCE_IN_INCHES, VELOCITY);
 
-        //  drive backward 12 inches ;
-        telemetry.addData("Reverse:  ", "back 6 inches");
-        telemetry.update();
-        this.driveReverse(DISTANCE_IN_INCHES, Constants.DEFAULT_VELOCITY);
-        sleep(SLEEP_TIME);
+            telemetry.addData("Strafe:  ", "strafe left");
+            telemetry.update();
+            this.strafeLeft(DISTANCE_IN_INCHES, 0.50 * Constants.DEFAULT_VELOCITY);
+            sleep(SLEEP_TIME);
 
-        //  Strafe 12 inches right ; both values must be negative ;
-        telemetry.addData("Strafe:  ", "strafe 6 inches right");
-        telemetry.update();
-        this.strafeRight(DISTANCE_IN_INCHES, 0.50 * Constants.DEFAULT_VELOCITY);
-        sleep(SLEEP_TIME);
+            //  drive backward 12 inches ;
+            telemetry.addData("Reverse:  ", "back 6 inches");
+            telemetry.update();
+            this.driveReverse(DISTANCE_IN_INCHES, Constants.DEFAULT_VELOCITY);
+            sleep(SLEEP_TIME);
 
-        //  Turn 90 degrees to the right
-        telemetry.addData("GyroTurn:  right ", "90 degrees");
-        telemetry.update();
-        this.turnRight(TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
-        telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
-        telemetry.update();
-        sleep(SLEEP_TIME);
+            //  Strafe 12 inches right ; both values must be negative ;
+            telemetry.addData("Strafe:  ", "strafe 6 inches right");
+            telemetry.update();
+            this.strafeRight(DISTANCE_IN_INCHES, 0.50 * Constants.DEFAULT_VELOCITY);
+            sleep(SLEEP_TIME);
 
-        //  Turn 90 degrees left;  note degrees direction and velocity
-        telemetry.addData("GyroTurn: left  ", "90 degrees??");
-        telemetry.update();
-        this.turnLeft(TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
-        telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
-        telemetry.update();
-        sleep(SLEEP_TIME);
+            //  Turn 90 degrees to the right
+            telemetry.addData("GyroTurn:  right ", "90 degrees");
+            telemetry.update();
+            this.turnRight(TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
+            telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
+            telemetry.update();
+            sleep(SLEEP_TIME);
 
-        telemetry.addData("GyroTurn:  right ", "90X2 degrees");
-        telemetry.update();
-        this.turnRight(2 * TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
-        telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
-        telemetry.update();
-        sleep(SLEEP_TIME);
+            //  Turn 90 degrees left;  note degrees direction and velocity
+            telemetry.addData("GyroTurn: left  ", "90 degrees??");
+            telemetry.update();
+            this.turnLeft(TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
+            telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
+            telemetry.update();
+            sleep(SLEEP_TIME);
 
-        //  Turn 90 degrees left;  note degrees direction and velocity
-        telemetry.addData("GyroTurn: left  ", "90X2 degrees??");
-        telemetry.update();
-        this.turnLeft(2 * TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
-        telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
-        telemetry.update();
-        sleep(SLEEP_TIME);
+            telemetry.addData("GyroTurn:  right ", "90X2 degrees");
+            telemetry.update();
+            this.turnRight(2 * TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
+            telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
+            telemetry.update();
+            sleep(SLEEP_TIME);
 
+            //  Turn 90 degrees left;  note degrees direction and velocity
+            telemetry.addData("GyroTurn: left  ", "90X2 degrees??");
+            telemetry.update();
+            this.turnLeft(2 * TURN_ANGLE, Constants.MOTOR_MAX_VELOCITY);
+            telemetry.addData("GyroTurn:  Done, ", imu.getAngle());
+            telemetry.update();
+            sleep(SLEEP_TIME);
+        }
+
+        this.stopOpMode();
+
+        this.addTelemetry();
+        telemetry.update();
     }
 }
