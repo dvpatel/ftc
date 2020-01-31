b3bb4f3bf0546ef34def4d7c37713ce92d3b0440
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-31 04:26:40 -0500

    Update.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoryTest.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoryTest.java
@@ -1,41 +1,53 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.blueprint.ftc.core.TrajectoryFactory;
 import org.roadrunner.mecanum.SampleMecanumDriveREV;
 import org.roadrunner.util.AssetsTrajectoryLoader;
 
 import java.io.IOException;
 
+@Config
 @Autonomous()
 //@Disabled
 public class TrajectoryTest extends LinearOpMode {
 
+    /*
+      "BlueFoundation", "BlueLoadingStone1", "BlueLoadingStone2", "BlueLoadingStone3",
+      "RedFoundation", "RedLoadingStone1", "RedLoadingStone2", "RedLoadingStone3";
+     */
+
+    public static String TRAJECTORY_NAME = "RedLoadingStone3";
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
-        Trajectory traj = null;
-        try {
-            traj = AssetsTrajectoryLoader.load("SimpleTest") ;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        SampleMecanumDriveREV drive = new SampleMecanumDriveREV(hardwareMap);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        if (traj != null) {
-            SampleMecanumDriveREV drive = new SampleMecanumDriveREV(hardwareMap);
+        try {
+
+            // x, y, heading;
+            //  Starting pose;
+            //  drive.setPoseEstimate(new Pose2d(-32.88, -63.13, -4.101523742186674));
+
+            Trajectory traj = TrajectoryFactory.getInstance().getTrajectory(TRAJECTORY_NAME);
             drive.followTrajectorySync(traj);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
 
     }
 }
