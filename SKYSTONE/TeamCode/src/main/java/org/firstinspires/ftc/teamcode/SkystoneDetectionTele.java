385d4756dcca2b005bc8910a379221740758db38
Dipesh Patel <mr.dvpatel@gmail.com>
2020-01-31 04:23:22 -0500

    Update Skystone logic.

--- SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionTele.java
+++ SKYSTONE/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionTele.java
@@ -1,59 +1,48 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.path.Path;
+import com.acmerobotics.roadrunner.path.PathBuilder;
+import com.acmerobotics.roadrunner.path.heading.LinearInterpolator;
+import com.acmerobotics.roadrunner.trajectory.BaseTrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryGenerator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.blueprint.ftc.core.AbstractLinearOpMode;
 import org.blueprint.ftc.core.Constants;
 import org.blueprint.ftc.core.IntakeSystem;
 import org.blueprint.ftc.core.SkystoneDetector;
+import org.blueprint.ftc.core.TrajectoryFactory;
+import org.roadrunner.DriveConstants;
+import org.roadrunner.mecanum.SampleMecanumDriveREV;
+
+import java.io.IOException;
 
 
 //  See
 //  https://github.com/gearsincorg/FTCVuforiaDemo/blob/master/TeleopOpmode.java
 
-@TeleOp(name = "SkystoneTele")
-@Disabled
-public class SkystoneDetectionTele extends AbstractLinearOpMode {
+@Autonomous(name = "SkystoneDetection")
+//  @Disabled
+public class SkystoneDetectionTele extends BaseAutonomous {
 
     private SkystoneDetector skystoneDetector;
     private IntakeSystem intakeSystem;
 
-    private static final double VELOCITY = 0.35 * Constants.MOTOR_MAX_VELOCITY;  // ticks per second
-    double[] targetCoordinates;
-    boolean foundStone;
+    private boolean isBlue;
 
-    private ElapsedTime runtime = new ElapsedTime();
+    double[] targetCoordinates;
+    int detectedStoneNumber;
+
+    //  Game quadrant;
+    private GameQuadrant quadrant;
+
+    private boolean quadrantSelected;
+
 
     @Override
     public void initOpMode() throws InterruptedException {
@@ -61,136 +50,138 @@ public class SkystoneDetectionTele extends AbstractLinearOpMode {
 
         this.intakeSystem = this.rosie.getIntakeSystem();
 
+        //  this.detectSkystoneOnField();
         this.skystoneDetector = this.rosie.getSkystoneDetector();
-        this.skystoneDetector.initVuforia(this);
-        this.skystoneDetector.activateTracking();
 
-        //  wait for camera to be activated;
-        sleep(10000);
+        this.setupZone();
 
-        //  detect during init;  give 30 seconds;
-        runtime.reset();
-        while ((runtime.seconds() < 30.0)) {
-            foundStone = this.detectSkystone();
-            if (!foundStone) {
-                telemetry.addData("Skystone", " searching.");
-                telemetry.addData("Time remaining", runtime);
-                telemetry.update();
-            } else {
+        detectedStoneNumber = this.skystoneDetector.detectSkystoneOnField(this, isBlue);
+    }
+
+    private void setupZone() throws InterruptedException{
+        //  Select quadrant;
+        this.quadrant = this.selectGameQuadrant();
+        telemetry.addData("Selected Zone:  ", quadrant.toString());
+        telemetry.addData("Robot Direction (- left, + right):  ", GameQuadrant.direction(quadrant));
+        telemetry.addData("Mode:  ", "init complete;  Running");
+        telemetry.update();
+
+        switch(quadrant) {
+            case LOADING_RED:
+                isBlue = false;
                 break;
-            }
-
-            //  this.skystoneDetector.addNavTelemetry();
-            idle();
+            case LOADING_BLUE:
+                isBlue = true;
+                break;
+            case BUILDING_BLUE:
+            case BUILDING_RED:
+                throw new InterruptedException("Robot cannot be in building zone.  Start again.");
         }
-        this.skystoneDetector.deactivateTracking();
-
-        if (foundStone) {
-            double dY = targetCoordinates[1];
-            telemetry.addData("Skystone", " Found it.");
-            telemetry.addData("dY", dY);
-            telemetry.update();
-
-            sleep(5000);
-        }
-
     }
 
     @Override
     public void stopOpMode() {
-        // Disable Tracking when we are done;
-        this.skystoneDetector.deactivateTracking();
         this.intakeSystem.stop();
     }
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
         this.initOpMode();
 
-        // Wait for the game to start (driver presses PLAY)
-        while (!isStarted()) {
-            // Prompt User
-            telemetry.addData(">", "Press start");
-
-            // Display any Nav Targets while we wait for the match to start
-            this.skystoneDetector.targetsAreVisible();
-            this.skystoneDetector.addNavTelemetry();
-            telemetry.update();
-        }
+        telemetry.addData("Stone Number", detectedStoneNumber);
+        telemetry.update();
 
         this.waitToPressStart();
 
-        while(opModeIsActive()) {
+        //  Intake system too high, also can't be down before start of match or in Init.  Refs don't like this.
+        this.intakeSystem.setIntakeServosInitPosition();
 
-            if (this.foundStone) {
-                this.driveToTarget(this.targetCoordinates);
+        //  Force value
+        detectedStoneNumber = 1;
 
-                //  this.intakeSystem.start();
+        if (opModeIsActive()) {
 
-                stop();
+            if (isBlue) {
+                //  this.blueMission(detectedStoneNumber);
+            } else {
+                //  this.redMission(detectedStoneNumber);
             }
 
+
+            //  this.goToBlueStone(detectedStoneNumber);
+            //  this.pickupBlueStone();
+
+
+            //  over the stone
+            //  this.intakeSystem.start();
+            //  move down inch?
         }
 
         this.stopOpMode();
     }
 
-    private boolean detectSkystone() {
-        if (skystoneDetector.targetsAreVisible()) {
-            String targetName = skystoneDetector.getTargetName();
-            if (Constants.VISIBLE_TARGET_NAME.equals(targetName)) {
+    private void blueMission(int stoneNumber) {
+        String trajName = "BlueLoadingStone"+stoneNumber;
+        this.goToStone(trajName);
 
-                // robotBearing:  yaw / heading;
-                //  targetRange:  Hypotenuse
-                //  targetBearing:  Angle between x and y
-                //  relativeBearing:  targetBearing-robotBearing
-                // robotX, robotY, robotX, robotBearing, targetRange, targetBearing, relativeBearing;
-                this.targetCoordinates = this.skystoneDetector.getTargetCoordinatesInInches();
-                return true;
-            }
-        }
-        return false;
+        //  Next step;
     }
 
-    private void driveToTarget(double[] targetCoordinates) {
+    private void redMission(int stoneNumber){
+        String trajName = "RedLoadingStone"+stoneNumber;
+        this.goToStone(trajName);
 
-        double dX = Math.abs(targetCoordinates[0]);
+        //  Next step;
+    }
+
+    private void goToStone(String trajName) {
+        try {
+            SampleMecanumDriveREV drive = new SampleMecanumDriveREV(hardwareMap);
+            Trajectory traj = TrajectoryFactory.getInstance().getTrajectory(trajName);
+            drive.followTrajectorySync(traj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void pickupBlueStone() {
+        this.intakeSystem.start();
+        this.driveReverse(2);  //  2 inches;
+        this.intakeSystem.stop();
+    }
+
+    //  Not dynamic trajectory;
+    private void blueLoadingStone() {
+
+        double dX = Math.abs(targetCoordinates[0]) - 2.25;
         double dY = targetCoordinates[1];
-        double angle = targetCoordinates[5];
-        double thetaRadians = Math.toRadians(90 - Math.abs(angle));
-        double hypot = targetCoordinates[4];
-        double totalY = hypot * Math.cos(thetaRadians);
 
-        //  intake offset;
-        double buffer = 2.0;
-        double intakeOffset = 3.25;
-        double robotWidth = Constants.DRIVETRAIN_WIDTH / 2;
-        double stoneOffset = 1.0;
+        SampleMecanumDriveREV drive = new SampleMecanumDriveREV(hardwareMap);
 
+        BaseTrajectoryBuilder baseTraj = drive.trajectoryBuilder()
+                .forward(dX);
 
-        double distX = dX;
-        double distY = 0;
-        if (dY > 0) {  //  right side of camera origin;
-            distY = robotWidth + totalY+buffer;
-        } else if (dY < 0) {  //  left side of camera origin
-            distY = robotWidth - totalY + stoneOffset;
-        }
-
-        telemetry.addData("Angle", angle);
-        telemetry.addData("TotalY", totalY);
-        telemetry.addData("Y", dY);
-        telemetry.update();
-
-        //  direction;  Negative is left of robot;  Positive is right of robot
-        //  positive velocity strafe left; negative strafe right
         if (dY > 0) {
-            this.strafeRight(distY, VELOCITY);
-        } else if (dY < 0) {
-            this.strafeLeft(distY, VELOCITY);
+            baseTraj = baseTraj.strafeRight(Math.abs(dY) + Constants.DRIVETRAIN_WIDTH);
+        } else {
+            baseTraj = baseTraj.strafeLeft(Math.abs(dY) + Constants.DRIVETRAIN_WIDTH);
         }
 
-        this.drive(distX, VELOCITY);
+        Trajectory traj = baseTraj.build();
+        drive.followTrajectorySync(traj);
 
+    }
+
+    private void blueLoadingStoneLast() {
+        SampleMecanumDriveREV drive = new SampleMecanumDriveREV(hardwareMap);
+
+        double dX = 24.0 + (24.0 - Constants.DRIVETRAIN_LENGTH);
+        double dY = 20.0 + (Constants.DRIVETRAIN_WIDTH / 2);
+
+        Trajectory traj = drive.trajectoryBuilder().forward(dX).strafeRight(dY).build();
+        drive.followTrajectorySync(traj);
     }
 }
